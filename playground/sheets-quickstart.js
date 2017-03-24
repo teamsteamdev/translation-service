80b3e98fe1c958bd0c88fc6e587d367baa96a9ca
John Steam <teamsteamdev@gmail.com>
2017-03-24 15:39:04 -0500

    Add Google Sheets Quickstart

---
+++ playground/sheets-quickstart.js
@@ -0,0 +1,121 @@
+const fs = require('fs')
+const readline = require('readline')
+const google = require('googleapis')
+const GoogleAuth = require('google-auth-library')
+
+// If modifying these scopes, delete your previously saved credentials
+// at ~/.credentials/sheets.googleapis.com-nodejs-quickstart.json
+const SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+const TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE) + '/.credentials/'
+const TOKEN_PATH = TOKEN_DIR + 'sheets.googleapis.com-nodejs-quickstart.js'
+
+// Load client secrets from a local file.
+fs.readFile('playground/client_secret.json', function processClientSecrets (err, content) {
+  if (err) {
+    console.log('Error loading client secret file: ' + err)
+    return
+  }
+
+  authorize(JSON.parse(content), listMajors)
+})
+
+/**
+ * Create an OAuth2 client with the given credentials, and then execute the
+ * given callback function.
+ *
+ * @param {Object} credentials The authorization client credentials.
+ * @param {function} callback The callback to call with the authorized client.
+ */
+function authorize (credentials, callback) {
+  const clientSecret = credentials.installed.client_secret
+  const clientId = credentials.installed.client_id
+  const redirectUrl = credentials.installed.redirect_uris[0]
+  let auth = new GoogleAuth()
+  let oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl)
+
+  // Check if we have previously stored a token.
+  fs.readFile(TOKEN_PATH, (err, token) => {
+    if (err) {
+      getNewToken(oauth2Client, callback)
+    } else {
+      oauth2Client.credentials = JSON.parse(token)
+      callback(oauth2Client)
+    }
+  })
+}
+
+/**
+ * Get and store new token after prompting for user authorization, and then
+ * execute the given callback with the authorized OAuth2 client.
+ *
+ * @param {google.auth.OAuth2} oauth2Client The OAuth2 client to get token for.
+ * @param {getEventsCallback} callback The callback to call with the authorized
+ *     client.
+ */
+function getNewToken (oauth2Client, callback) {
+  const authUrl = oauth2Client.generateAuthUrl({
+    access_types: 'offline',
+    scope: SCOPES
+  })
+  console.log('Authorize this app by visiting this url: ', authUrl)
+  let rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  })
+  rl.question('Enter the code from that page here: ', (code) => {
+    rl.close()
+    oauth2Client.getToken(code, (err, token) => {
+      if (err) {
+        console.log('Error while trying to retrieve access token', err)
+        return
+      }
+      oauth2Client.credentials = token
+      storeToken(token)
+      callback(oauth2Client)
+    })
+  })
+}
+
+/**
+ * Store token to disk be used in later program executions.
+ *
+ * @param {Object} token The token to store to disk.
+ */
+function storeToken (token) {
+  try {
+    fs.mkdirSync(TOKEN_DIR)
+  } catch (err) {
+    if (err.code !== 'EEXIST') {
+      throw err
+    }
+  }
+  fs.writeFileSync(TOKEN_PATH, JSON.stringify(token))
+  console.log('Token stored to ' + TOKEN_PATH)
+}
+
+/**
+ * Print the names and majors of students in a sample spreadsheet:
+ * https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
+ */
+function listMajors (auth) {
+  let sheets = google.sheets('v4')
+  sheets.spreadsheets.values.get({
+    auth: auth,
+    spreadsheetId: '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms',
+    range: 'Class Data!A2:E'
+  }, (err, response) => {
+    if (err) {
+      console.log('The API returned an error: ' + err)
+      return
+    }
+    let rows = response.values
+    if (rows.length === 0) {
+      console.log('No data found.')
+    } else {
+      console.log('Name, Major:')
+      rows.forEach((row) => {
+        console.log(`${row[0]}, ${row[4]}`)
+      })
+    }
+  })
+}
